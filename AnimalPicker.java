4c2373ad8fdd00ba31117497b6449709e0c1a340
PeterZegarek <petrzegarek@gmail.com>
2024-02-23 12:43:43 -0500

    Step three

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,7 +4,9 @@ public class AnimalPicker{
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
 
-        System.out.println("Which animal would you like to see? A dog or a cat? Type 'D' for dog or 'C' for cat.");
+        boolean selected = false;
+        while (!selected){
+            System.out.println("Which animal would you like to see? A dog or a cat? Type 'D' for dog or 'C' for cat.");
             String input = sc.nextLine();
             if (input.equals("D")){
                 System.out.println("Dog ASCII");
@@ -15,6 +17,8 @@ public class AnimalPicker{
             else {
                 System.out.println("Please enter a valid input");
             }
+        
+        }
         
 
         
