1e700104b80a6815ea43d320bfc93978560763f7
PeterZegarek <petrzegarek@gmail.com>
2024-02-23 12:18:44 -0500

    Step one

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,7 +4,15 @@ public class AnimalPicker{
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
 
-    
+        System.out.println("Which animal would you like to see? A dog or a cat? Type 'D' for dog or 'C' for cat.");
+            String input = sc.nextLine();
+            if (input.equals("D")){
+                System.out.println("Dog ASCII");
+            }
+            else if (input.equals("C")){
+                System.out.println("Cat ASCII");
+            }
+        
         
 
         
